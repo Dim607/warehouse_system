fa73373c2f5485eec1f5d9ce6c2f8a3f36071c60
Dim607 <188760755+Dim607@users.noreply.github.com>
2025-08-14 18:24:39 +0300

    Added field sold_quantity Modified field quantity as int Created the following methods: 1) sell_product: which updates the product based on the amount sold 2) sort_name: sorts list of broducts based on their name 3) sort_sold_quantity: sorts list of products based on the quantity

--- model/product.py
+++ model/product.py
@@ -1,5 +1,6 @@
 from __future__ import annotations  # for pyright typechecking
 import uuid
+from operator import attrgetter
 
 
 """
@@ -14,8 +15,8 @@ class Product:
         self,
         id: str,
         name: str,
-        quantity: float,  # maybe int
-        sold_quantity: float,
+        quantity: int,  # maybe int
+        sold_quantity: int,
         weight: float,
         volume: float,
         category: str,  # TODO maybe add as collection to db
@@ -26,8 +27,9 @@ class Product:
     ):
         self.id: str               = id if id is not None else str(uuid.uuid4())
         self.name:str              = name 
-        self.quantity:float        = quantity
-        self.weight:float          = weight
+        self.quantity: int         = quantity
+        self.sold_quantity: int    = sold_quantity
+        self.weight: float         = weight
         self.volume: float         = volume
         self.category: str         = category
         self.purchase_price: float = purchase_price
@@ -85,3 +87,22 @@ class Product:
             data.get("manufacturer"),
             data.get("unit_gain"),
         )
+
+    def sell_product(self, sold_product: int):
+        self.sold_quantity = self.sold_quantity + sold_product
+        self.quantity      = self.quantity - sold_product
+        self.unit_gain     = self.selling_price * sold_product
+
+
+    # Sorts in place based on product name
+    # if reverse = true -> sort in descending order
+    @staticmethod
+    def sort_name(product_list: list[Product], reverse: bool):
+        product_list.sort(key=attrgetter("name"), reverse=reverse)        
+
+
+    # Sorts in place based on product sold quantity
+    # if reverse = true -> sort in descending order
+    @staticmethod
+    def sort_sold_quantity(product_list: list[Product], reverse: bool):
+        product_list.sort(key=attrgetter("sold_quantity"), reverse=reverse)        
