d43008e36a53bcf694556e69072e0f247cddc69d
Dim607 <188760755+Dim607@users.noreply.github.com>
2025-08-15 12:23:20 +0300

    Added supervisor class

---
+++ model/supervisor.py
@@ -0,0 +1,36 @@
+from model.employee import Employee
+
+
+class Supervisor(Employee):
+    def __init__(
+        self,
+        id: str | None,
+        name: str,
+        surname: str,
+        username: str,
+        password: str,
+        unit_id: str,
+        unit_name: str,
+    ):
+        super().__init__(
+            id,
+            name,
+            surname,
+            username,
+            password,
+            unit_id,
+            unit_name,
+        )
+
+
+    def create_employee(self, name:str, surname: str, username: str, password: str) -> Employee:
+        employee = Employee(
+            id        = None,
+            name      = name,
+            surname   = surname,
+            username  = username,
+            password  = password,
+            unit_id   = self.unit_id,
+            unit_name = self.unit_name,
+        )
+        return employee
