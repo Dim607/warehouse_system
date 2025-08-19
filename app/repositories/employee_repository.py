fc70fa8fafe7629707f340572bcb696aed301fc6
Dim607 <188760755+Dim607@users.noreply.github.com>
2025-08-19 18:25:17 +0300

    added function that queries db with user id and returns employee with that id

--- app/repositories/employee_repository.py
+++ app/repositories/employee_repository.py
@@ -45,14 +45,24 @@ class EmployeeRepository:
     def __init__(self, employee_collection: Collection) -> None:
         self.employee_collection = employee_collection
 
+
+    def get_employee_by_id(self, id: str) -> Employee | None:
+        result = self.employee_collection.find({"id": id})
+        if result is None:
+            return None
+
+        return Employee.from_dict(result)
+
+
     # Returns the employee whose username and password match the function parameters
     # If there is no employee it returns None
     # If an Employee object cannot be created from the data in the db an exception is raised
-    def get_employee(self, username: str, password: str) -> Employee | None:
+    def get_employee(self, username: str, password: str, unit_id) -> Employee | None:
         query = {
             "$and": [
                 {"username": username},
                 {"password": password},
+                {"unit_id" : unit_id},
             ]
         }
         result = self.employee_collection.find_one(query)
