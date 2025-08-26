e1935b6961d40e257cb8f816fbf5131a802c6be4
Dim607 <188760755+Dim607@users.noreply.github.com>
2025-08-26 22:07:32 +0300

    minor changes

--- server.py
+++ server.py
@@ -3,5 +3,4 @@ from app import create_server
 server = create_server()
 
 if __name__ == "__main__":
-    # server.run(debug=True, host=SERVER_HOST, port=SERVER_PORT)
     server.run(debug=True, host=server.config["SERVER_HOST"], port=server.config["SERVER_PORT"])
