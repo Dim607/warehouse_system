fced01aeaca360c3a307b9ffe61e21a43da2afca
Dim607 <188760755+Dim607@users.noreply.github.com>
2025-09-06 00:16:56 +0300

    Made the following changes: 1) Modified get_employee-by_id() and get_employee() to use    Employee.from_persistence_dict() 2) Added documentation

--- app/repositories/employee_repository.py
+++ app/repositories/employee_repository.py
@@ -1,5 +1,6 @@
 from typing import List, Optional
 from pymongo.database import Collection
+from pymongo.results import InsertManyResult, InsertOneResult
 from app.model.employee import Employee
 
 
@@ -15,19 +16,51 @@ class EmployeeRepository:
 
 
     def get_employee_by_id(self, id: str) -> Optional[Employee]:
+        """
+        Get an Employee instance from the DB by ID.
+
+        Args:
+            id (str): The ID of the employee to retrieve.
+
+        Returns:
+            Employee | None:
+            - An Employee object if found. Note that `unit_name` is not stored in the database.
+            - None if no employee with the given ID exists.
+
+        Raises:
+            ValueError: If the Database record is missing required attributes
+            (see Employee.from_persistence_dict() for details on the required attributes).
+        """
         query = {"id": id, "role": "employee"}
         result = self.user_collection.find_one(query)
 
         if result is None:
             return None
 
-        return Employee.from_dict(result)
+        return Employee.from_persistence_dict(result)
 
 
-    # Returns the employee whose username and password match the function parameters
-    # If there is no employee it returns None
-    # If an Employee object cannot be created from the data in the db an exception is raised
     def get_employee(self, username: str, password: str, unit_id: str) -> Optional[Employee]:
+        """
+        Retrieve an Employee instance from the DB using their credentials.
+
+        Note that `unit_name` is not stored in the DB and it will be set to None.
+
+        Args:
+            username (str): The `username` of the employee.
+            password (str): The `password` of the employee.
+            unit_id (str): The `id` of the unit the employee is assigned to.
+
+        Returns:
+            Employee | None:
+            - An Employee object if found. Note that `unit_name` is not stored in the database.
+            - None if no employee with the given ID exists.
+
+        Raises:
+            ValueError: If the Database record is missing required attributes
+            (see Employee.from_persistence_dict() for details on the required attributes).
+        """
+
         query = {
             "username": username,
             "password": password,
@@ -40,47 +73,38 @@ class EmployeeRepository:
         if result is None:
             return None
 
-        return Employee.from_dict(result)
+        return Employee.from_persistence_dict(result)
 
 
-    def insert_employee(
-        self,
-        name: str,
-        surname: str,
-        username: str,
-        password: str,
-        unit_id: str,
-        unit_name: str
-    ):
-        # create an employee object to validate fields
-        employee = Employee.from_dict({
-            "name":      name,
-            "surname":   surname,
-            "username":  username,
-            "password":  password,
-            "unit_id":   unit_id,
-            "unit_name": unit_name,
-        })
+    def insert_employee(self, employee: Employee) -> InsertOneResult:
+        """
+        Inserts an employee to the database
 
-        # turn the previously created employee object into a dict and insert it to the user collection
-        result = self.user_collection.insert_one(employee.to_dict())
+        Removes the field `unit_name` from `employee`, 
+        since only the `unit_id` is needed.
 
-        return {
-            "acknowledged": result.acknowledged,
-            "inserted_id": str(result.inserted_id)
-        }
+        Args:
+            employee (Employee): The employee to insert.
+
+        Returns:
+            pymongo.results.InsertOneResult: The result of the insertion.
+        """
+        return self.user_collection.insert_one(employee.to_percistance_dict())
 
 
-    def insert_employees(self, employees: List[Employee]):
-        to_be_inserted = []
-        for employee in employees:
-            try:
-                emp = Employee.from_dict(employee)
-                to_be_inserted.append(emp.to_dict())
-            except Exception as e:  # if one employee has wrong format stop
-                raise ValueError(f"Invalid employee format: {employee}") from e
+    def insert_employees(self, employees: List[Employee]) -> InsertManyResult:
+        """
+        Inserts a employees to the database
 
-        result = self.user_collection.insert_many(to_be_inserted)
+        Removes the field `unit_name` from each employee` inemployees`,
+        only the `unit_id` is needed
 
-        return result
+        Args:
+            employees (List[Employee]): A list with the employees to insert
+
+        Returns:
+            pymongo.results.InsertOneResult: The result of the insertion
+        """
+        return self.user_collection.insert_many([e.to_percistance_dict() for e in employees])
+
 
