c410e951d65bb457fc02c347fd2a4ce024ef0b6f
Dim607 <188760755+Dim607@users.noreply.github.com>
2025-09-05 23:43:18 +0300

    Made the followin changes: 1) Created helper method _from_dict() 2) Modified from_dict to call _from_dict() 3) Created from persistence_dict() which creates a dictionary element    from a Database document where unit_name is not stored

--- app/model/employee.py
+++ app/model/employee.py
@@ -1,5 +1,5 @@
 from  __future__ import annotations # for pyright typechecking
-from typing import Any, Optional
+from typing import Any, Dict, List, Optional
 import uuid
 
 
@@ -13,7 +13,7 @@ class Employee:
     # unit_name is not saved in the database for each employee
     # because it can be found using unit_id
     # keep it here as a help field
-    unit_name: str
+    unit_name: Optional[str]
     role: str = "employee"
 
     def __init__(
@@ -24,7 +24,7 @@ class Employee:
         username: str,
         password: str,
         unit_id: str,
-        unit_name: str,
+        unit_name: Optional[str],
     ):
         self.id           = id if id is not None else str(uuid.uuid4())
         self.name         = name
@@ -91,24 +91,101 @@ class Employee:
 
 
     @classmethod
-    def from_dict(cls, data):
-        # id can be None so dont include it in the attr list
-        attributes = ["name", "surname", "username", "password", "unit_id", "unit_name"]
+    def from_dict(cls, data: Dict[str, Any]) -> Employee:
+        """
+        Returns an Employee instance from a dictionary
 
-        for attr in attributes:
+
+        Use this method to create an Employee object where `unit_name` is allowed to be missing.
+
+        This method validates that all required attributes are present and non-None.
+        Raises ValueError if any required attribute is missing.
+
+        Args:
+            data (dict): Dictionary containing the employee attributes.
+            The following keys are required:
+            - `name`
+            - `surname`
+            - `username`
+            - `password`
+            - `unit_id`
+            - `unit_name`
+
+        Returns:
+            Employee: An Employee instance initialize with the given attributes
+
+        Raises:
+            ValueError: If any required attribute is missing or None.
+        """
+
+        # id can be None so dont include it in the attr list
+        required_attrs = ["name", "surname", "username", "password", "unit_id", "unit_name"]
+        return cls._from_dict(data, required_attrs)
+
+
+    @classmethod
+    def from_persistence_dict(cls, data: Dict[str, Any]) -> Employee:
+        """
+        Returns an Employee instance from a dictionary.
+
+        Use this method to create an Employee object from a DB document 
+        where `unit_name` is allowed to be missing.
+
+        This method validates that all required attributes are present and non-None.
+        Raises ValueError if any required attribute is missing.
+
+        Args:
+            data (dict): Dictionary containing the employee attributes.
+            The following keys are required:
+            - `name`
+            - `surname`
+            - `username`
+            - `password`
+            - `unit_id`
+
+        Returns:
+            Employee: An Employee instance initialized with the given attributes
+
+        Raises:
+            ValueError: If any required attribute is missing or None.
+        """
+        required_attrs = ["name", "surname", "username", "password", "unit_id"]
+        return cls._from_dict(data, required_attrs)
+
+
+    @classmethod
+    def _from_dict(cls, data: dict[str, Any], required_attrs: List[str]) -> Employee:
+        """
+        Returns an Employee instance from a dictionary
+
+        Validates that all required attributes are present and non-None.
+        Raises ValueError if any required attribute is missing.
+
+        Args:
+            data (dict): Dictionary containing the employee attributes.
+            require_attrs (List[str]): A list containing the names of all the required attributes.
+
+        Returns:
+            Employee: An Employee instance initialized with the given attributes
+
+        Raises:
+            ValueError: If any required attribute is missing or None.
+        """
+        for attr in required_attrs:
             if data.get(attr) is None:
-                raise Exception(f"Attribute {attr} cannot be None")
+                raise ValueError(f"Attribute {attr} cannot be None")
 
         employee = cls(
             id        = data.get("id"),
-            name      = data.get("name"),
-            surname   = data.get("surname"),
-            username  = data.get("username"),
-            password  = data.get("password"),
-            unit_id   = data.get("unit_id"),
+            name      = str(data["name"]),
+            surname   = str(data["surname"]),
+            username  = str(data["username"]),
+            password  = str(data["password"]),
+            unit_id   = str(data["unit_id"]),
             unit_name = data.get("unit_name"),
         )
         return employee
+
 
     def change_password(self, new_password: str) -> bool:
         if self.password == new_password:
