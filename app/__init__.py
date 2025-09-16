42950edd7b7ba3153c15c421b842d7f399f987a4
Dim607 <188760755+Dim607@users.noreply.github.com>
2025-09-16 16:37:36 +0300

    added code for supervisor to insert user to database

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 from app.blueprints.auth import create_auth_blueprint
 from app.blueprints.employee import create_employee_blueprint
 from app.blueprints.product import create_product_blueprint
+from app.blueprints.supervisor import create_supervisor_blueprint
 from app.blueprints.user import create_user_blueprint
 from app.custom_flask import CustomFlask
 from app.model.unit import Unit
@@ -10,10 +11,12 @@ from app.repositories import product_repository
 from app.repositories import unit_repository
 from app.repositories.employee_repository import EmployeeRepository
 from app.repositories.product_repository import ProductRepository
+from app.repositories.supervisor_repository import SupervisorRepository
 from app.repositories.unit_repository import UnitRepository
 from app.repositories.user_repository import UserRepository
 from app.services.employee_service import EmployeeService
 from app.services.product_service import ProductService
+from app.services.supervisor_service import SupervisorService
 from app.services.user_service import UserService
 
 
@@ -54,7 +57,7 @@ def create_server():
 
     # Initialize repositories
     emp_repo = EmployeeRepository(server.user_collection)
-    # sup_repo = 
+    sup_repo = SupervisorRepository(server.user_collection)
     # adm_repo = 
     unt_repo = UnitRepository(unit_collection)
     prd_repo = ProductRepository(server.product_collection)
@@ -62,6 +65,7 @@ def create_server():
 
     # Initialize services
     employee_service = EmployeeService(usr_repo, emp_repo, unt_repo)
+    supervisor_service = SupervisorService(usr_repo, emp_repo, sup_repo, unt_repo)
     user_service     = UserService(usr_repo, unt_repo)
     product_service  = ProductService(prd_repo, unt_repo)
 
@@ -70,5 +74,6 @@ def create_server():
     server.register_blueprint(create_employee_blueprint(employee_service, user_service))
     server.register_blueprint(create_user_blueprint(user_service))
     server.register_blueprint(create_product_blueprint(product_service))
+    server.register_blueprint(create_supervisor_blueprint(supervisor_service, employee_service, user_service))
 
     return server
---
+++ app/blueprints/supervisor/__init__.py
@@ -0,0 +1,67 @@
+from typing import Optional
+from flask import Blueprint, flash, redirect, render_template, request, session, url_for
+from app.blueprints.names import SUPERVISOR_BP
+from app.exceptions.exceptions import UnitNotFoundByIdError
+from app.services.employee_service import EmployeeService
+from app.services.supervisor_service import SupervisorService
+from app.services.user_service import UserService
+from app.utils.auth_utils import login_required, required_role
+
+
+def create_supervisor_blueprint(
+    sup_service: SupervisorService,
+    emp_service: EmployeeService,
+    user_service: UserService
+) -> Blueprint:
+    supervisor_bp = Blueprint(SUPERVISOR_BP, __name__, template_folder="templates")
+
+    @supervisor_bp.route("/employee/create", methods=["GET", "POST"])
+    @login_required
+    @required_role("supervisor")
+    def create_employee():
+        create_employee_page = "supervisor/create_employee.html"
+
+        if request.method != "POST":
+            return render_template(create_employee_page)
+
+        # get the field value or ""
+        name: str     = request.form.get("name", "").strip()
+        surname: str  = request.form.get("surname", "").strip()
+        username: str = request.form.get("username", "").strip()
+        password: str = request.form.get("password", "").strip()
+        unit_id: str  = session["unit_id"]
+
+        # if any variable had incorrect value
+        if not all((name, surname, username, password)):
+            return render_template(
+                create_employee_page,
+                error="All fields are required.",
+                name=name,
+                surname=surname,
+                username=username,
+            )
+
+        try: 
+            insert_result = emp_service.insert_employee(
+                name, surname, username, password, unit_id
+            )
+        except UnitNotFoundByIdError:
+            return render_template(
+                create_employee_page,
+                error="Could not find your unit.",
+                name=name,
+                surname=surname,
+                username=username,
+            )
+        except ValueError:
+            return render_template(
+                create_employee_page,
+                error="The user's record in the database is missing required attributes.",
+            )
+
+        flash("Employee created successfully.", "success")
+
+        return redirect(url_for("supervisor.create_employee"))
+
+
+    return supervisor_bp
