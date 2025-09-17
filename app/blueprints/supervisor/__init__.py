48956de5f68cddbbb1652336d6e1f56c11456b46
Dim607 <188760755+Dim607@users.noreply.github.com>
2025-09-17 15:59:19 +0300

    added view/delete employees route for supervisor

--- app/blueprints/supervisor/__init__.py
+++ app/blueprints/supervisor/__init__.py
@@ -1,8 +1,10 @@
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
 from flask import Blueprint, flash, redirect, render_template, request, session, url_for
 from pymongo.errors import DuplicateKeyError
 from app.blueprints.names import SUPERVISOR_BP
-from app.exceptions.exceptions import UnitNotFoundByIdError
+from app.exceptions.exceptions import UnitNotFoundByIdError, UserNotFoundByIdError
+from app.model.employee import Employee
+from app.services import employee_service
 from app.services.employee_service import EmployeeService
 from app.services.supervisor_service import SupervisorService
 from app.services.user_service import UserService
@@ -10,8 +12,8 @@ from app.utils.auth_utils import login_required, required_role
 
 
 def create_supervisor_blueprint(
-    sup_service: SupervisorService,
-    emp_service: EmployeeService,
+    supervisor_service: SupervisorService,
+    employee_service: EmployeeService,
     user_service: UserService
 ) -> Blueprint:
     supervisor_bp = Blueprint(SUPERVISOR_BP, __name__, template_folder="templates")
@@ -24,8 +26,8 @@ def create_supervisor_blueprint(
         unit_id: str
     ) -> Optional[str]:
         error: Optional[str] = None
-        try: 
-            insert_result = emp_service.insert_employee(
+        try:
+            insert_result = employee_service.insert_employee(
                 name, surname, username, password, unit_id
             )
         except UnitNotFoundByIdError:
@@ -34,6 +36,18 @@ def create_supervisor_blueprint(
             error="A user with the same username already exists in the unit."
         except ValueError:
             error="The user's record in the database is missing required attributes."
+        return error
+
+
+    def _try_delete_employee(employee_id: str, unit_id: Optional[str]):
+        error: Optional[str] = None
+        try:
+            employee_service.delete_employee_by_id(employee_id, unit_id)
+        except UnitNotFoundByIdError:
+            error="Could not find your unit."
+        except UserNotFoundByIdError:
+            error = "Could not find employee."
+
         return error
 
 
@@ -77,6 +91,42 @@ def create_supervisor_blueprint(
         flash("Employee created successfully.", "success")
 
         return redirect(url_for("supervisor.create_employee"))
+
+
+    @supervisor_bp.route("/employees", methods=["GET", "POST"])
+    @login_required
+    @required_role("supervisor")
+    def view_employees():
+        # view all employees in unit, can select and delete employee
+        view_employees_page       = "supervisor/view_employees.html"
+        employees: List[Employee] = []
+        unit_id: Optional[str]    = session.get("unit_id")
+        error: Optional[str]      = None
+
+        if unit_id is not None:
+            employees = employee_service.get_employees_in_unit(unit_id)
+
+        if request.method != "POST":
+            return render_template(view_employees_page, employees=employees)
+
+        employee_id = request.form.get("employee_id", "").strip()
+
+        if not employee_id:
+            return render_template(
+                view_employees_page,
+                error="This employee does not exist.",
+                employees=employees,
+            )
+
+        error = _try_delete_employee(employee_id, unit_id)
+
+        if error is not None:
+            return render_template(
+                view_employees_page, error=error, employees=employees
+            )
+
+        return redirect(url_for("supervisor.view_employees"))
+
 
 
     return supervisor_bp
